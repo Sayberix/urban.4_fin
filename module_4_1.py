8a61086da2b83b9a942048d8efb13201ea37f5de
Sayberix <mr.maaks@gmail.com>
2024-12-12 03:48:56 +0300

    first and fin commit
    The task is completed

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as fk
+from true_math import divide as tr
+
+result1 = fk(5,2)
+result2 = fk(16,0)
+result3 = tr(26,3)
+result4 = tr(18,0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
